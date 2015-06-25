d4430566ce6b45be10d1bd3ce7bfd4650c708dab
unknown <guilherme.ferro@SEINFO-143578.quinino.fiocruz.br>
2015-06-24 21:11:18 -0300

    Salvando os passos da tarefa extraclasse

--- src/main/java/filmoteca/servico/FilmeServico.java
+++ src/main/java/filmoteca/servico/FilmeServico.java
@@ -1,9 +1,14 @@
 package filmoteca.servico;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +18,24 @@ import filmoteca.entidade.Filme;
 @Service
 @EnableAutoConfiguration
 public class FilmeServico {
+    
+    private JdbcTemplate jdbcTemplate;
+    
+    
+    @Autowired
+    public FilmeServico(JdbcTemplate jdbcTemplate) {
+        super();
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-	public List<Filme> obterFilmes() {
-		List<Filme> filmes = new ArrayList<Filme>();
-		filmes.add( new Filme("Star Wars", "George Lucas", Classificacao.MEDIA));
-		filmes.add( new Filme("Superman", "Spilberg", Classificacao.ALTA));
-		filmes.add( new Filme("Batman", "Oliver Stone", Classificacao.ALTA));
-		return filmes;
-	}
+    public List<Filme> obterFilmes() {
+        return jdbcTemplate.query("select * from Filme", new RowMapper<Filme>() {
+            @Override
+            public Filme mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new Filme(rs.getString("titulo"), 
+                        rs.getString("diretor"), 
+                        Classificacao.valueOf(rs.getString("classificacao")));
+            }            
+        });
+    }
 }
